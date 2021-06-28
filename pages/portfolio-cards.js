f5c5a13ff3bb8cd294f938884e2a5b4bb23f80b2
nakamori-naoya <nmk623tk@gmail.com>
2021-06-28 13:22:19 +0900

    テスト３

--- pages/portfolio-cards.js
+++ pages/portfolio-cards.js
@@ -1,25 +1,33 @@
 import { getPortfolios } from '../lib/portfolio';
 import Layout from "../components/Layout";
 import PortfolioListContainer from '../components/PortfolioList/PortfolioListContainer';
+import  axios from 'axios';
 
-// export async function getStaticProps() {
-//   const staticfilteredPortfolios = await getPortfolios(); 
-//   return {
-//     props: { staticfilteredPortfolios }, 
-//     revalidate: 10, //ISR
-//   };
-// }
+export async function getStaticProps() {
+  const res = await axios
+  .get(`http://localhost:3000/api/v1/portfolios`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+      }
+    }
+  )
+const  staticfilteredPortfolios = res.data
+  return {
+    props: { staticfilteredPortfolios }, 
+    revalidate: 10, //ISR
+  };
+}
 
 
-//引数{staticfilteredPortfolios}
-export default function PortfolioIndex() {
+
+export default function PortfolioIndex({staticfilteredPortfolios}) {
   return(
-    <></>
-      // <Layout title="profile-editor">
-      //   <PortfolioListContainer 
-      //     staticfilteredPortfolios={staticfilteredPortfolios}
-      //   />
-      // </Layout>
+      <Layout title="profile-editor">
+        <PortfolioListContainer 
+          staticfilteredPortfolios={staticfilteredPortfolios}
+        />
+      </Layout>
   )
 }
 
