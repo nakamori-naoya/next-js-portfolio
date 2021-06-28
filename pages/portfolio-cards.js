81ff5994d06af278de7c1fffd7c8854338573bdb
nakamori-naoya <nmk623tk@gmail.com>
2021-06-28 10:49:31 +0900

    a

--- pages/portfolio-cards.js
+++ pages/portfolio-cards.js
@@ -1,23 +1,23 @@
-import { getPortfolios } from '../lib/portfolio';
-import Layout from "../components/Layout";
-import PortfolioListContainer from '../components/PortfolioList/PortfolioListContainer';
+// import { getPortfolios } from '../lib/portfolio';
+// import Layout from "../components/Layout";
+// import PortfolioListContainer from '../components/PortfolioList/PortfolioListContainer';
 
-export async function getStaticProps() {
-  const staticfilteredPortfolios = await getPortfolios(); 
-  return {
-    props: { staticfilteredPortfolios }, 
-    revalidate: 10, //ISR
-  };
-}
+// export async function getStaticProps() {
+//   const staticfilteredPortfolios = await getPortfolios(); 
+//   return {
+//     props: { staticfilteredPortfolios }, 
+//     revalidate: 10, //ISR
+//   };
+// }
 
-export default function PortfolioIndex({staticfilteredPortfolios}) {
-  return(
-      <Layout title="profile-editor">
-        <PortfolioListContainer 
-          staticfilteredPortfolios={staticfilteredPortfolios}
-        />
-      </Layout>
-  )
-}
+// export default function PortfolioIndex({staticfilteredPortfolios}) {
+//   return(
+//       <Layout title="profile-editor">
+//         <PortfolioListContainer 
+//           staticfilteredPortfolios={staticfilteredPortfolios}
+//         />
+//       </Layout>
+//   )
+// }
 
 
